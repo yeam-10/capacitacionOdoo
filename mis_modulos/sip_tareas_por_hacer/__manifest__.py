36b59742094fc39ebf07dca86647a9c2eaa74e37
Yeam <anyeimarrequena@gmail.com>
2020-06-24 20:33:17 -0400

    in

---
+++ mis_modulos/sip_tareas_por_hacer/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+
+'name': "sip_tareas_por_hacer",
+
+'summary': """ Soluciones  integrales, Pyme C.A""",
+
+'description': """ Modulo estandar para modificar el funcionamiento de account""",
+
+'author': " anye",
+'website': " https://yeam-10.github.io/Portafolio-yeam/ ",
+'version': '1.0 ',
+'depends': ['base'], 
+
+ 'installable': True,
+ 'application': False,
+ 'auto_install': False,
+
+
+
+
+}
