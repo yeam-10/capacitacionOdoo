d535cf86cb74ce0878e7f3a72fa6f8c074e9fc45
yeam-10 <61568058+yeam-10@users.noreply.github.com>
2020-07-28 00:43:36 -0400

    AYUDA error de sintaxis de python linea 22 super

---
+++ mis_modulos/todo_app/todo_app/todo_task_models/todo_task.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+from odoo import models, fields, api
+class TodoTask(models.Model):
+    _inherit = 'sip.tareas'
+    user_id = fields.Many2one('res.users', 'Responsible')
+    date_deadline = fields.Date('Deadline')
+
+    name = fields.Char(help="What needs to be done?")
+
+    @api.multi
+    def sip_clear_done(self):
+        domain = [('is_done', '=', True), '|', ('user_id', '=', self.env.uid), ('user_id', '=', False)]
+        done_recs = self.search(domain)
+        done_recs.write({'active': False})
+        return True
+
+    @api.one
+    def sip_done(self):
+        if self.user_id != self.env.user:
+           raise Exception('Only the responsible can do this!')
+        else:
+           return super(TodoTask, self).sip_done() 
