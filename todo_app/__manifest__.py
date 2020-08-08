d7f29eae506d67cf70ab7d1c2239bc79ee90ec22
yeam-10 <61568058+yeam-10@users.noreply.github.com>
2020-08-08 17:50:09 -0400

    MOD Ampliar las vistas completado
    Heredamos una vista del modulo de sip tareas

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Multiuser To Do',
+    'summary':"""Yeam Developer Group""",
+
+    'description':"""Modulo que extiende de la app tareas por hacer""",
+
+    'author':'Anye',
+    'website':" https://yeam-10.github.io/Portafolio-yeam/ ",
+  
+    'depends': ['base'],
+
+    'data':[
+        'todo_task_view/todo_task.xml',
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+
+}
